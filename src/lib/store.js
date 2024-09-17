23a6992ef8c9decda9136ec75b224706331d62c8
Mikhail Katsupko <t63723@gmail.com>
2024-09-17 11:58:33 +0700

    add telegram

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,3 +1,4 @@
 import { writable } from 'svelte/store';
 
 export const selectedReportType = writable(0);
+export const userStore = writable({});
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,40 @@
-<script>
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { userStore } from "$lib/store.js";
 	import ProfileRoute from "$lib/components/ProfileRoute.svelte";
 
 	const telegramId = 123;
+
+	let isTelegramWebApp: boolean | undefined = false;
+	let webAppData: any = null;
+	let userInfo: any | undefined;
+	let err: string | undefined | null;
+
+	onMount(async () => {
+		isTelegramWebApp = window.Telegram && window.Telegram.WebApp;
+		webAppData = window.Telegram?.WebApp;
+		userInfo = webAppData?.initDataUnsafe?.user;
+
+		console.log(isTelegramWebApp);
+		console.log(webAppData);
+		console.log(userInfo);
+
+		if (userInfo != undefined) {
+			userStore.set(userInfo);
+		}
+	});
 </script>
 
 <div class="app">
 	<ProfileRoute {telegramId} />
+
+	<div>
+		<pre>
+			{isTelegramWebApp}
+			{webAppData}
+			{userInfo}
+		</pre>
+	</div>
 
 	<main>
 		<slot />
