45a4d1f50e1767bdac1e42d91226077f82b73ae5
Mikhail Katsupko <t63723@gmail.com>
2024-09-26 11:19:16 +0700

    в навигацию добавил явный переход

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,13 +1,26 @@
 <script>
 	import { page } from "$app/stores";
+	import { goto } from "$app/navigation";
 </script>
 
 <header>
 	<nav class="nav">
 		<div class="nav-center">
-			<a href="/" class:active={$page.url.pathname === '/'}>home</a>
-			<a href="/reports" class:active={$page.url.pathname === '/reports'}>reports</a>
-			<a href="/config" class:active={$page.url.pathname === '/config'}>config</a>
+			<a
+				href="/"
+				on:click={() => goto("/")}
+				class:active={$page.url.pathname === "/"}>home</a
+			>
+			<a
+				href="/reports"
+				on:click={() => goto("/reports")}
+				class:active={$page.url.pathname === "/reports"}>reports</a
+			>
+			<a
+				href="/config"
+				on:click={() => goto("/config")}
+				class:active={$page.url.pathname === "/config"}>config</a
+			>
 		</div>
 	</nav>
 </header>
